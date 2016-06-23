91fa6a8df4b38f382eaec92f2ab17ef223664b65
Mengqiu <zhuran94@163.com>
2016-06-23 18:06:31 +0800

    [Mengqiu] init project by react and redux

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src/app');
+const BUILD_DIR = path.resolve(__dirname, 'src/public');
+
+const config = {
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test:/\.jsx?$/,
+                include: APP_DIR,
+                loader: 'babel',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['react', 'es2015']
+                }
+            }
+
+        ]
+    }
+
+};
+
+module.exports = config; 
